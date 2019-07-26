cb8147277968869f2965a09ecd03208006832d25
almaaro <alvar.marti@gmail.com>
2019-07-26 22:37:09 +0300

    removed psp offset from stabilized flight mode

--- src/modules/fw_att_control/FixedwingAttitudeControl.cpp
+++ src/modules/fw_att_control/FixedwingAttitudeControl.cpp
@@ -160,7 +160,7 @@ FixedwingAttitudeControl::vehicle_manual_poll()
 					_att_sp.roll_body = constrain(_att_sp.roll_body,
 								      -radians(_param_fw_man_r_max.get()), radians(_param_fw_man_r_max.get()));
 
-					_att_sp.pitch_body = -_manual.x * radians(_param_fw_man_p_max.get()) + radians(_param_fw_psp_off.get());
+                                        _att_sp.pitch_body = -_manual.x * radians(_param_fw_man_p_max.get());
 					_att_sp.pitch_body = constrain(_att_sp.pitch_body,
 								       -radians(_param_fw_man_p_max.get()), radians(_param_fw_man_p_max.get()));
 
