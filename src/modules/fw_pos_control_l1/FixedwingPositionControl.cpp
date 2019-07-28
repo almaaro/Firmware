48e6d1b807e298d2a0acb6314345cd90a267a771
almaaro <alvar.marti@gmail.com>
2019-07-28 14:17:23 +0300

    fix

--- src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
+++ src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -1353,7 +1353,7 @@ FixedwingPositionControl::control_landing(const Vector2f &curr_pos, const Vector
 	 * Checking for land_noreturn to avoid unwanted climb out
 	 */
 
-        if ((_current_altitude < _landingslope.flare_relative_alt() + terrain_alt) &&
+        if (_land_noreturn_vertical || (_current_altitude < _landingslope.flare_relative_alt() + terrain_alt) &&
 	    (wp_distance < _landingslope.flare_length() - _land_touchdown_point_shift) &&
             ((_param_fw_lnd_req_terr.get() && _time_last_t_alt > 0 && _land_rngfnd_bump_handled)
              || !_param_fw_lnd_req_terr.get() || !_param_fw_lnd_useter.get())) {
@@ -1414,8 +1414,6 @@ FixedwingPositionControl::control_landing(const Vector2f &curr_pos, const Vector
 		 * if current position is below the slope continue at previous wp altitude
 		 * until the intersection with slope
 		 * */
-
-		_land_noreturn_vertical  = false;
 
 		float altitude_desired = terrain_alt;
 
